58bf705b296ccb0be6aee23b4f7296f596789f81
GLV-off <glv.off@mail.ru>
2025-05-08 11:37:16 +0300

    Start

---
+++ Src/Glv.Testing.Cross.pas
@@ -0,0 +1,33 @@
+unit Glv.Testing.Cross;
+
+{$I 'test.inc'}
+{$M+}
+
+interface
+
+uses
+{$IFDEF FPC}
+  FpcUnit,
+  TestRegistry;
+{$ELSE FPC}
+  TestFramework;
+{$ENDIF FPC}
+
+type
+  TCrossTestCase = TTestCase;
+  TCrossTestCaseClass = class of TTestCase;
+
+procedure CrossRegTest(ClassOfTest: TCrossTestCaseClass; const Subpath: string = '');
+
+implementation
+
+procedure CrossRegTest(ClassOfTest: TCrossTestCaseClass; const Subpath: string);
+begin
+{$IFDEF FPC}
+  RegisterTest(Subpath, ClassOfTest);
+{$ELSE FPC}
+  RegisterTest(ClassOfTest.Suite);
+{$ENDIF FPC}
+end;
+
+end.
